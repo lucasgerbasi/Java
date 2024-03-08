f818d1ac133888ce6cd865e65fac4118c93415f1
Lucas Gerbasi <126926247+lucasgerbasi@users.noreply.github.com>
2024-03-08 20:18:00 -0300

    Update ShoppingCart.java

--- Lista3/src/ShoppingCart.java
+++ Lista3/src/ShoppingCart.java
@@ -7,6 +7,54 @@ public class ShoppingCart {
     private List<CarItem> carItens;
 
     public ShoppingCart() {
+        // aloca espaço na memória para o vetor
         this.carItens = new ArrayList<CarItem>();
+    }
+    public ShoppingCart(int id, float totalPrice) {
+        this.setId(id);
+        this.setTotalPrice(totalPrice);
+        // aloca espaço na memória para o vetor
+        this.carItens = new ArrayList<CarItem>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public List<CarItem> getCarItens() {
+        return carItens;
+    }
+
+    public void setCarItens(List<CarItem> carItens) {
+        this.carItens = carItens;
+    }
+
+    // método para adicionar um CarItem no vetor
+    // caracteriza uma composição, pois CarItem é criado
+    // em função do ShoppingCart
+    public void addCarItem(int id, int quantity, Product product){
+        // cria um CarItem e adiciona este no vetor
+        this.carItens.add(new CarItem(id, quantity, product));
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" +
+                "id=" + id +
+                ", totalPrice=" + totalPrice +
+                ", carItens=" + carItens +
+                '}';
     }
 }
