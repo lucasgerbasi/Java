4a823ce124ab89158b39be2fa972563fdc764e72
Lucas Gerbasi <126926247+lucasgerbasi@users.noreply.github.com>
2024-03-21 22:05:05 -0300

    Add files via upload

---
+++ ListaHeranca/Ex2/Main.java
@@ -0,0 +1,8 @@
+package ListaHeranca.Ex2;
+
+public class Main {
+    public static void main(String[] args) {
+        Aniversario c1 = new Aniversario("afafsdgd");
+        c1.showMessage();
+    }
+}
